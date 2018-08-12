3f54aa16cb54aa2fac5da552d845d19643e62d07
shadowman <agascompany@gmail.com>
2018-08-12 18:35:47 +0300

    dateFormatter

--- Smack/View/MessageCell.swift
+++ Smack/View/MessageCell.swift
@@ -31,6 +31,19 @@ class MessageCell: UITableViewCell {
         userImg.image = UIImage(named: message.userAvatar)
         userImg.backgroundColor =
         USerDataService.instance.returnUIColor(componens: message.userAvatarColor)
+        
+        guard var isoDate = message.timeStamp else {return}
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStamp.text = finalDate
+        }
     }
 
 }
